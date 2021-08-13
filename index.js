09003ed1e5bcc32fbb41713c2cc06fa6126c7243
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-13 13:18:38 -0300

    Principal JS

---
+++ index.js
@@ -0,0 +1,8 @@
+// Teste principal do uso de JQUERY;
+// inclusão do JQUERY via cdn do google : "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+// acima da inclusão do arquivo JavaScript;
+
+// primeiros testes com Jquery
+// uso do $
+
+
