770be495212707a4587a36d23e7c8f141b6c286f
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-14 11:28:11 -0300

    1.2

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 // primeiros testes com Jquery
 // uso do $
 
+
+/*
 $(function(){
     //qualquer função
     //será carregada antes dos elementos da página
@@ -14,3 +16,28 @@ $(window).load(function(){
     //qualquer função
     //será carregada APÓS os elementos da página carregarem por completo
 })
+
+*/
+
+/*****************************************************************************************/
+
+
+// funções do Jquery.
+// sempre começar com $() e dentro do parenteses colocar entre aspas simples o que quer modificar
+// $('p').efeitoJquery
+
+
+// $('h1').css("color","red");
+// $('p').css("weight","300");
+// $('h1').hide(); ---> faz o elemento começar escondido
+// $('h2').delay(2000) ----> milisegundos
+
+
+// testando:
+    $(document).ready(function(){
+        $('h1').css("color","red");
+        $('p').css("color","bold");
+        $('h1').hide(3000);
+        $('h2').delay(1000);
+    })
+
