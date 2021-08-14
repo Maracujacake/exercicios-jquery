c9563ef43bb44879fc1a781b8974b28dd78a5734
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-14 11:45:06 -0300

    1.4

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ $(window).load(function(){
 
 */
 
-/*****************************************************************************************/
+    /*****************************************************************************************/
 
 
 // funções do Jquery.
@@ -32,7 +32,7 @@ $(window).load(function(){
 // $('h1').hide(); ---> faz o elemento começar escondido
 // $('h2').delay(2000) ----> milisegundos
 
-/*****************************************************************************************/
+    /*****************************************************************************************/
 
 // testando:
     $(document).ready(function(){
@@ -43,12 +43,12 @@ $(window).load(function(){
         $('h2').fadeIn("slow");
     })
 
-/*****************************************************************************************/
+    /*****************************************************************************************/
 
     // $('elemento').fadeIn("tempo"); --> aparecimento do texto
     // $('elemento').text("texto a ser mostrado a partir de agora"); --> muda o texto do elemento
 
-/*****************************************************************************************/
+    /*****************************************************************************************/
 
     // encadeando funções do Jquery:
 
@@ -61,3 +61,31 @@ $(window).load(function(){
     $(document).ready(function(){
         $('p').css("color","red").text("abluababa").delay(2000);
     })
+
+    /*****************************************************************************************/
+
+    // encadeamento em bloco:
+
+     
+            $(document).ready(function(){
+                $('p').css("color","red")
+                .text("abluababa")
+                .delay(2000);
+            })
+    
+// cada função em uma linha;
+// facilita a análise;                
+
+
+    /*****************************************************************************************/
+
+    // função de click Jquery;
+    // ação muito poderosa que realiza uma função ao clique;
+
+    $(document).ready(function(){
+        $('h2').click(function(){
+            $('body').css("background","blue");
+        })
+    })
+
+    // função que muda a cor do fundo ao clicar no h2
